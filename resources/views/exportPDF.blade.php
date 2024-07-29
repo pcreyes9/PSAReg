6160d733edde5ee7d83b563db8271b9e0d21f2f1
Paul Reyes <97373132+pcreyes9@users.noreply.github.com>
2024-07-29 09:39:27 +0800

    Update exportPDF.blade.php

--- resources/views/exportPDF.blade.php
+++ resources/views/exportPDF.blade.php
@@ -43,7 +43,7 @@
             </h3>
             <br>
             <div class="text-center">
-                <img class="img w-3/4" src='storage/photos/proof of payments/{{ $infos->proof_payment }}'   alt="">
+                <img class="img" style="height: 300px; width: auto" src='storage/photos/proof of payments/{{ $infos->proof_payment }}'   alt="">
             </div>
             <div class="page-break"></div>
         @endforeach
