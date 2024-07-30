1c5b5ea590ec623a7c2a70376035535e3d820a46
Paul Reyes <97373132+pcreyes9@users.noreply.github.com>
2024-07-30 13:25:15 +0800

    Update view-mem-reg.blade.php TRY IMG URL

--- resources/views/livewire/view-mem-reg.blade.php
+++ resources/views/livewire/view-mem-reg.blade.php
@@ -245,7 +245,7 @@
                                             <td class="px-6 py-4 text-center">
                                                 Senior ID
                                                 <a href="{{ url('/admin/viewMemReg/download/senior/' . $regs->senior_citizen) }}">
-                                                    <img class="pt-2  h-auto max-w-lg mx-auto text-center hover:w-full w-14" src="{{ url('storage/photos/senior ids/'. $regs->senior_citizen) }}" alt="Logo">
+                                                    <img class="pt-2  h-auto max-w-lg mx-auto text-center hover:w-full w-14" src="{{ url('storage/app/photos/senior ids/'. $regs->senior_citizen) }}" alt="Logo">
                                                 </a>
                                             </td>
                                         @else
