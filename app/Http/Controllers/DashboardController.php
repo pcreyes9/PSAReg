8b195e6da868f81a3fc9bb3db9d9c9635934107e
pcreyes9 <pcreyes09@gmail.com>
2024-07-30 10:40:46 +0800

    admin dashboard update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,15 +13,36 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class DashboardController extends Controller
 {
-    public $re, $info;
+    public $faceRM, $onlineRM, $totRM, $faceLM, $onlineLM, $totLM, $faceTM, $onlineTM, $totTM, $faceNM, $onlineNM, $totNM,$sumFace, $sumOnline, $totalReg;
 
     public function dashboard(){
-        
-        $id = Auth::user()->psa_id;
-        // $info = Members::where('PSA_ID', Auth::user()->psa_id)->get();
+        $faceRM = DB::table('registrations')->where('reg_type', 'regFace')->where('membership', 'RM')->count();
+        $onlineRM = DB::table('registrations')->where('reg_type', 'regOnline')->where('membership', 'RM')->count();
+        $totRM = DB::table('registrations')->where('membership', 'RM')->count();
 
-        // dd($info->toArray());
-        return view("user_account.dashboard");
+        $faceLM = DB::table('registrations')->where('reg_type', 'regFace')->where('membership', 'LM')->count();
+        $onlineLM = DB::table('registrations')->where('reg_type', 'regOnline')->where('membership', 'LM')->count();
+        $totLM = DB::table('registrations')->where('membership', 'LM')->count();
+
+        $faceTM = DB::table('registrations')->where('reg_type', 'regFace')->where('membership', 'TM')->count();
+        $onlineTM = DB::table('registrations')->where('reg_type', 'regOnline')->where('membership', 'TM')->count();
+        $totTM = DB::table('registrations')->where('membership', 'TM')->count();
+
+        $faceNM = DB::table('registrations')->where('reg_type', 'regFace')->where('membership', 'NON-MEMBER')->count();
+        $onlineNM = DB::table('registrations')->where('reg_type', 'regOnline')->where('membership', 'NON-MEMBER')->count();
+        $totNM = DB::table('registrations')->where('membership', 'NON-MEMBER')->count();
+
+
+
+        $sumFace = DB::table('registrations')->where('reg_type', 'regFace')->count();
+        $sumOnline = DB::table('registrations')->where('reg_type', 'regOnline')->count();
+        $totalReg = DB::table('registrations')->count();
+
+        return view("user_account.dashboard", compact('sumFace', 'sumOnline', 'totalReg', 
+        'faceRM', 'onlineRM', 'totRM',
+        'faceLM', 'onlineLM', 'totLM',
+        'faceTM', 'onlineTM', 'totTM',
+        'faceNM', 'onlineNM', 'totNM',));
     }
     public function viewMemReg (){
         // $reg = Registration::all()->orderBy('id');
