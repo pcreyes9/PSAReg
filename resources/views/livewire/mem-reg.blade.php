90b170564bbaf507288cabac7a93487bfdae9e86
pcreyes9 <pcreyes09@gmail.com>
2024-07-31 09:37:23 +0800

    psa id checker upd

--- resources/views/livewire/mem-reg.blade.php
+++ resources/views/livewire/mem-reg.blade.php
@@ -1,7 +1,31 @@
 
+
 <form wire:submit.prevent="submit">
     {{-- <div wire:loading>LOADING...</div> --}}
     @csrf
+
+    <div class="my-5">
+        <button type="button" class="btn btn-dark solid blank" wire:click="showChecker">PSA ID Checker</button>
+        @if ($show)
+            <div class="row mt-5">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label style=" font-weight: 750; font-size: medium; color:white;">Enter Last Name:</label>
+                        <input style="box-shadow: 2px 2px 3px gray; background-color: white; color: black; font-weight: bold;" class="form-control form-control-subject" placeholder="" wire:model.live='name'>
+                    </div>
+                    <button type="button" class="btn btn-primary solid blank mb-3" wire:click="checker" style="background: #d6cb00; color: #000066">Check</button>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                    <label style=" font-weight: 750; font-size: medium; color:white;">PSA ID</label>
+                    @if ($this->res != null)
+                        <textarea class="form-control " rows="6" style="box-shadow: 2px 2px 3px gray; background-color: white; color: black; font-weight: bold; text-align:left;" id="message-text" readonly>{{ implode("\n", $res)}}</textarea>
+                    @endif
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
     <div class="error-container"></div>
     <div class="row">
         <div class="col-md-2">
@@ -157,36 +181,15 @@
         </div> --}}
         
         <div class="text-right"><br>
-            <button type="button" class="btn btn-dark solid blank" wire:click="showChecker">PSA ID Checker</button>
             <button wire:loading.attr="disabled" type="submit" class="btn btn-primary solid blank" style="background: #d6cb00; color: #000066">
                 {{-- <div class="loader"></div> --}}
                 <svg wire:loading xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity="0.25"/><path fill="currentColor" d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>
                 <div wire:loading.remove>
                     Register
                 </div>
-                
             </button>
         </div>
     </div>
-    @if ($show)
-        <div class="row mt-5">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label style=" font-weight: 750; font-size: medium; color:white;">Enter Last Name:</label>
-                    <input style="box-shadow: 2px 2px 3px gray; background-color: white; color: black; font-weight: bold;" class="form-control form-control-subject" placeholder="" wire:model.live='name'>
-                </div>
-                <button type="button" class="btn btn-primary solid blank mb-3" wire:click="checker" style="background: #d6cb00; color: #000066">Check</button>
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                <label style=" font-weight: 750; font-size: medium; color:white;">PSA ID</label>
-                @if ($this->res != null)
-                    <textarea class="form-control " rows="6" style="box-shadow: 2px 2px 3px gray; background-color: white; color: black; font-weight: bold; text-align:left;" id="message-text" readonly>{{ implode("\n", $res)}}</textarea>
-                @endif
-                </div>
-            </div>
-        </div>
-    @endif
 </form>
 
 
