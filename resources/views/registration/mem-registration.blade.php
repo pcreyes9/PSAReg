22e9b8ac844addd52d3c7403da8a175fc03ca2a4
pcreyes9 <pcreyes09@gmail.com>
2024-07-31 10:02:02 +0800

    conz req upd

--- resources/views/registration/mem-registration.blade.php
+++ resources/views/registration/mem-registration.blade.php
@@ -11,9 +11,8 @@
         <div class="row justify-content-center mb-2" data-animation-in="slideInLeft" data-duration-in="1.2">
             <div class="col-lg-12" >
                 <h3 class="into-sub-title" style="color: white">PSA MEMBERS REGISTRATION</h3>
-                <h5 style="color: white; font-style: italic; "><strong>Forget PSA ID? </strong>Click PSA ID Checker button. 
-                    <br>
-                    Enter your last name, and your PSA ID number will appear.</h5>
+                <h5 style="color: white; font-style: italic; "><strong>Forget PSA ID? </strong>Click PSA ID Checker button. </h5>
+                <p class="mt-n2" style="color: white; font-style: italic; ">Enter your last name, and your PSA ID number will appear.</p>
             </div>
             @if (session('success'))
                 <div class="alert alert-success">
