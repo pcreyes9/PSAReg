d14bdb8ca43c2f84f3f429e8377d9a56470ceb69
pcreyes9 <pcreyes09@gmail.com>
2024-08-30 11:25:16 +0800

    maintenance upd

--- resources/views/home/pages/registration-details.blade.php
+++ resources/views/home/pages/registration-details.blade.php
@@ -17,11 +17,12 @@
             <div class="col-lg-12 mb-3" >
                 <h3 class="into-sub-title" style="color: white">PSA 56<sup style="text-transform: lowercase">th</sup> Annual Convention Registration</h3>
                 <h3 class="section-header" style="color: white">27<sup style="text-transform: lowercase">th</sup> - 29<sup style="text-transform: lowercase">th</sup> <strong >November</strong>  2024 |  Manila Marriott Hotel</h3>
+                <h3 class="into-sub-title" style="color: white">WEBSITE IS UPDATING... KINDLY RETURN AFTER 10 MINS. THANK YOU!</h3>
             </div>
-            <div class="d-grid my-4">
+            {{-- <div class="d-grid my-4">
                 <a href="{{route('memReg')}}" class="btn btn-primary mt-3" style="font-size: 20px; background: #d6cb00; color: #000066">Members Registration</a>
                 <a href="{{route('nonMemReg')}}" class="btn btn-dark mt-3 ml-sm-3 " style="font-size: 20px;; ">Non-Members Registration</a>
-            </div>
+            </div> --}}
             <a class="gallery-popup wrapper" href="images/Annual Convention 2024 Registration Rates.png">
                 <img class="img-fluid w-75 zoom fade-img" style="box-shadow: 3px 3px 5px;" src="images/Annual Convention 2024 Registration Rates.png" alt="">
             </a>
