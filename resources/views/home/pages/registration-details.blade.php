64edd86377b7e096506716ba3722a7c9fd35063f
pcreyes9 <pcreyes09@gmail.com>
2024-08-30 11:25:57 +0800

    mtc upd

--- resources/views/home/pages/registration-details.blade.php
+++ resources/views/home/pages/registration-details.blade.php
@@ -19,6 +19,8 @@
                 <h3 class="section-header" style="color: white">27<sup style="text-transform: lowercase">th</sup> - 29<sup style="text-transform: lowercase">th</sup> <strong >November</strong>  2024 |  Manila Marriott Hotel</h3>
                 <h3 class="into-sub-title" style="color: white">WEBSITE IS UPDATING... KINDLY RETURN AFTER 10 MINS. THANK YOU!</h3>
             </div>
+            <br>
+            <br>
             {{-- <div class="d-grid my-4">
                 <a href="{{route('memReg')}}" class="btn btn-primary mt-3" style="font-size: 20px; background: #d6cb00; color: #000066">Members Registration</a>
                 <a href="{{route('nonMemReg')}}" class="btn btn-dark mt-3 ml-sm-3 " style="font-size: 20px;; ">Non-Members Registration</a>
