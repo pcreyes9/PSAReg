24193a102a6636894acc33c716c62d8b4eb02dbd
pcreyes9 <pcreyes09@gmail.com>
2024-10-03 10:24:50 +0800

    DB UPD

--- resources/views/registration/mem-registration.blade.php
+++ resources/views/registration/mem-registration.blade.php
@@ -8,7 +8,8 @@
 
 <section id="main-container" class="pb-2 main-container text-center project-area ">
     <div class="container">
-        <div class="row justify-content-center mb-2" data-animation-in="slideInLeft" data-duration-in="1.2">
+        <h3 class="into-sub-title pb-5" style="color: white">WEBSITE UPDATE INCOMING...</h3>
+        {{-- <div class="row justify-content-center mb-2" data-animation-in="slideInLeft" data-duration-in="1.2">
             <div class="col-lg-12" >
                 <h3 class="into-sub-title" style="color: white">PSA MEMBERS REGISTRATION</h3>
                 <h4 class="mt-5" style="color: white; font-style: italic; "><strong>Forgot your PSA ID NO.? </strong></h4>
@@ -22,7 +23,7 @@
             @endif
         </div>
         
-        <livewire:mem-reg/>
+        <livewire:mem-reg/> --}}
     </div>
 </section><!-- Main container end -->
 @endsection
